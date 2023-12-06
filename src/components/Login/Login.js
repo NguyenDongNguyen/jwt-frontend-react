b0d4ae5cc1d65a6ff6a442b8a3903e3e12c4c646
NguyenDongNguyen <ndn030802@gmail.com>
2023-12-06 23:43:56 +0700

    react router dom

---
+++ src/components/Login/Login.js
@@ -0,0 +1,10 @@
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            login
+            <button>test login</button>
+        </div>
+    );
+};
+
+export default Login;
