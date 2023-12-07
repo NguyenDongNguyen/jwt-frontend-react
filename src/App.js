8ad6ece0e75a1dfda597c042fde5ee4cda60cad9
NguyenDongNguyen <ndn030802@gmail.com>
2023-12-08 01:57:52 +0700

    validate form react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Login from "./components/Login/Login";
 import Nav from "./components/Navigation/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
     return (
@@ -19,6 +21,19 @@ function App() {
                     <Route path="*" element={"404 not found"} />
                 </Routes>
             </div>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </Router>
     );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,59 @@
 import "./Register.scss";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const Register = (props) => {
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     let history = useNavigate();
     const handleLogin = () => {
         history("/login");
+    };
+
+    useEffect(() => {
+        // axios
+        //     .get("http://localhost:8080/api/test-api")
+        //     .then((data) => console.log(">>> check axios: ", data));
+    }, []);
+
+    const isValidInputs = () => {
+        if (!email) {
+            toast.error("Email is required");
+            return false;
+        }
+        if (!phone) {
+            toast.error("Phone is required");
+            return false;
+        }
+        if (!password) {
+            toast.error("Password is required");
+            return false;
+        }
+        if (password !== confirmPassword) {
+            toast.error("Your password is not the same");
+            return false;
+        }
+
+        let regx = /\S+@\S+\.\S+/;
+        if (!regx.test(email)) {
+            toast.error("Please enter a valid email address");
+            return false;
+        }
+
+        return true;
+    };
+
+    const handleRegister = () => {
+        let check = isValidInputs();
+
+        let userData = { email, phone, username, password, confirmPassword };
+        console.log(">>> check user-data: ", userData);
     };
 
     return (
@@ -26,6 +75,8 @@ const Register = (props) => {
                                 type="text"
                                 className="form-control"
                                 placeholder="Email address"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
@@ -34,6 +85,8 @@ const Register = (props) => {
                                 type="text"
                                 className="form-control"
                                 placeholder="Phone number"
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
@@ -42,6 +95,8 @@ const Register = (props) => {
                                 type="text"
                                 className="form-control"
                                 placeholder="Username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
@@ -50,6 +105,8 @@ const Register = (props) => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
@@ -58,9 +115,18 @@ const Register = (props) => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Re-enter password"
+                                value={confirmPassword}
+                                onChange={(e) =>
+                                    setConfirmPassword(e.target.value)
+                                }
                             />
                         </div>
-                        <button className="btn btn-primary">Register</button>
+                        <button
+                            className="btn btn-primary"
+                            onClick={handleRegister}
+                        >
+                            Register
+                        </button>
                         <hr />
                         <div className="text-center">
                             <button
